18e06982777c7551872e17ea344e58ba286b8a26
tourpadaythar <tourpadaythar@gmail.com>
2026-07-10 12:58:34 +0630

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,7 @@
 
         document.querySelectorAll('.section').forEach(section => {
             section.classList.add('fade-in');
+                section.classList.add('visible');
             observer.observe(section);
         });
 
